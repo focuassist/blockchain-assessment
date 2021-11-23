78e7377711206da0eca094a3b6b182319a089d99
syamil <aimansyamilaziz@gmail.com>
2021-11-23 22:36:36 +0800

    Complete question 2

--- Question2/account.py
+++ Question2/account.py
@@ -1,12 +1,54 @@
+import itertools
 class Account:
+    newId = itertools.count().__next__
     def __init__(self, name, balance):
-        self.id
-        self.name
-        self.balance
+        self.id = Account.newId()
+        self.name = name    
+        self.balance = balance
 
     def deposit(self, amount):
+        self.balance+= amount
+        print(f"{self.name}'s account deposit {amount}")
+        return self.balance
 
     def withdraw(self, amount):
+        if self.balance>=amount:
+            self.balance-=amount
+            print(f"{self.name}'s account withdraw {amount}")
+        else:
+            print("\nInsufficient balance  ")
 
     def __str__(self):
         return f"{self.name}'s account. Balance: {self.balance}"
+
+#Creating a subclass
+class Account2(Account):
+    @property
+    def balance(self):
+        print("getter method called")
+        return self._balance
+
+    @balance.setter
+    def balance(self, amount):
+        print("setter method called")
+        self._balance = amount
+
+    def transfer(from_account, to_account, amount):
+        from_account.withdraw(amount)
+        to_account.deposit(amount)
+
+#Test variables
+obj1=Account("American Bank", 1000)
+obj2=Account2("RHB Bank", 1000)
+obj3=Account2("Maybank", 1000)
+List =[obj1, obj2]
+
+obj1.deposit(1000)
+obj1.withdraw(1000)
+obj2.deposit(1000)
+Account2.transfer(obj2, obj3, 1000)
+
+# print(vars(obj1))
+print(*List, sep="\tnote: ")
+print(List.insert(2,obj3))
+print(*List, sep="\tnote: ")
---
+++ Question2/readme.md
@@ -0,0 +1,9 @@
+# Why I choose List
+
+I choose a list because it keeps order while dictionary and set are do not.
+Lists are mutable, and hence, they can be altered even after their creation.
+The elements in the lists can be accessed via indices while dictionary using key-value. It is easier to do use indices since the id value is automatically increment from 0 the same as the indices value upon creating an object.
+
+Dictionary associates each key with a value, while list and set just contain values. It is very different use cases.
+
+Set requires items to be hashable, list doesn't. Since for this assignment the items can be both hashable and non-hashable, therefore, I prefer to use list.
